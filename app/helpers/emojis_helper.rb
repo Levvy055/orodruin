b6ea3e3954db73316712074197c8879a6d6272a8
Łukasz Niemier <lukasz@niemier.pl>
2015-05-06 13:38:51 +0200

    Fix emoji helper

--- app/helpers/emojis_helper.rb
+++ app/helpers/emojis_helper.rb
@@ -1,6 +1,6 @@
 module EmojisHelper
   def emoji_tag(emoji, size: 20, css_class: 'emoji')
-    image_tag("//#{Emoji.image_url_for_name(emoji['name'])}",
+    image_tag(Emoji.image_url_for_name(emoji['name']),
               alt: emoji['moji'],
               class: css_class,
               width: size,
